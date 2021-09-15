551d2717154279f96196806c4ef71d86de90bfa5
targusrock <targusrock@gmail.com>
2021-09-15 22:59:23 +0530

    Setup: Testing DB

--- backend/src/tests/setup.py
+++ backend/src/tests/setup.py
@@ -1,10 +1,44 @@
 import json
+import os
 
+from dotenv import load_dotenv
 from fastapi.testclient import TestClient
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
 
+from database.database import Base
+from routes import get_db
 from server import app
 
+load_dotenv()
+
+db_username = os.getenv("DB_USERNAME")
+db_password = os.getenv("DB_PASSWORD")
+db_server = os.getenv("DB_SERVER")
+db_name = os.getenv("DB_NAME_TESTING")
+
+SQLALCHEMY_DATABASE_URL = (
+    f"mysql+mysqlconnector://{db_username}:{db_password}@{db_server}/{db_name}"
+)
+
+engine = create_engine(SQLALCHEMY_DATABASE_URL)
+
+TestingSessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
 client = TestClient(app)
+
+Base.metadata.create_all(bind=engine)
+
+
+def override_get_db():
+    try:
+        db = TestingSessionLocal()
+        yield db
+    finally:
+        db.close()
+
+
+app.dependency_overrides[get_db] = override_get_db
 
 
 def create_test_client():
@@ -38,14 +72,11 @@ def delete_test_client():
     return response.status_code
 
 
-TOKEN = return_token()
-
-USER_TOKEN = TOKEN["access_token"]
-
-TOKEN_TYPE = TOKEN["token_type"]
-
-HEADERS = {"Authorization": f"{TOKEN_TYPE.capitalize()} {USER_TOKEN}"}
+token = return_token()
+user_token = token["access_token"]
+token_type = token["token_type"]
+headers = {"Authorization": f"{token_type.capitalize()} {user_token}"}
 
 
 def handle_urls(url):
-    return {"url": url, "headers": HEADERS, "allow_redirects": True}
+    return {"url": url, "headers": headers, "allow_redirects": True}
