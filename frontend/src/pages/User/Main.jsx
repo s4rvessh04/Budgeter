2bb31d6a3ef1924ae206acade461f834da38e827
targusrock <targusrock@gmail.com>
2021-12-18 18:20:43 +0530

    Updated: Functional HeroCards

--- frontend/src/pages/User/Main.jsx
+++ frontend/src/pages/User/Main.jsx
@@ -1,14 +1,19 @@
-import React, { useContext, useEffect, useRef, useState } from 'react';
-import { Redirect, useHistory } from 'react-router-dom';
+import React, {
+  useCallback,
+  useContext,
+  useEffect,
+  useRef,
+  useState,
+} from 'react';
+import { useHistory } from 'react-router-dom';
 import { Helmet } from 'react-helmet';
-import moment from 'moment';
 import * as Hi from 'react-icons/hi';
 import * as Fa from 'react-icons/fa';
 import * as Bi from 'react-icons/bi';
 import * as Ri from 'react-icons/ri';
+import moment from 'moment';
 
 import { useFetcher } from 'hooks';
-import { UserContext } from 'context';
 import { handleApiUrl } from 'shared';
 import {
   ErrorPromptAction,
@@ -18,36 +23,179 @@ import {
 } from 'components';
 
 export const Main = () => {
-  const [toggleSearch, setToggleSearch] = useState(false);
-  const [activeExpenseId, setActiveExpenseId] = useState(null);
+  const [borrow, setBorrow] = useState(0);
+  const [lend, setLend] = useState(0);
+  const [isLoading, setLoading] = useState(true);
+  const [headCards, setHeadCards] = useState([]);
   const [expenseData, setExpenseData] = useState([]);
-  const [, , isAuthenticated, ,] = useContext(UserContext);
+  const [userMaxExpense, setUserMaxExpense] = useState(0);
+  const [toggleSearch, setToggleSearch] = useState(false);
+  const [currentMonth] = useState(new Date().getMonth() + 1);
+  const [activeExpenseId, setActiveExpenseId] = useState(null);
+  const [lendBorrowToggle, setLendBorrowToggle] = useState(false);
+  const [expenseTotalAmount, setExpenseTotalAmount] = useState(0);
 
-  const { data, isLoading } = useFetcher({
-    url: handleApiUrl('/user/'),
+  const sharedExpenseFetcher = useFetcher({
+    url: handleApiUrl(`/expenses/shared?month=${currentMonth}`),
   });
+
+  const allSharedExpenseFetcher = useFetcher({
+    url: handleApiUrl(`/expenses/shared`),
+  });
+
+  const expenseFetcher = useFetcher({
+    url: handleApiUrl(`/expenses?month=${currentMonth}`),
+  });
+
+  const maxExpenseFetcher = useFetcher({
+    url: handleApiUrl('/max_expense/'),
+  });
+
+  const routerHistory = useHistory();
+
+  const handleLendBorrowToggle = () => {
+    setLendBorrowToggle(!lendBorrowToggle);
+  };
 
   useEffect(() => {
     const getExpenses = async () => {
-      const expenses = await data.expenses;
-      if (expenses) {
-        const allExpenses = expenses.concat(data.shared_expenses);
+      const expenses = await expenseFetcher.data;
+      const sharedExpenses = await sharedExpenseFetcher.data;
+      const maxExpense = await maxExpenseFetcher.data;
+      const allSharedExpense = await allSharedExpenseFetcher.data;
+
+      const calculateLending = (l) => {
+        let sum = 0;
+        for (let i of l) {
+          if (i.shared) {
+            for (let j of i.shared_expenses) {
+              sum += j.amount;
+            }
+          }
+        }
+        return sum;
+      };
+
+      setLend(calculateLending(expenses));
+
+      const calculateBorrowing = (l) => {
+        let sum = 0;
+        for (let i of l) {
+          sum += i.SharedExpense.amount;
+        }
+        return sum;
+      };
+
+      setBorrow(calculateBorrowing(allSharedExpense));
+
+      if (expenses && sharedExpenses) {
+        const allExpenses = expenses.concat(
+          sharedExpenses.map((i) => i.SharedExpense)
+        );
+        setExpenseTotalAmount(
+          allExpenses.length === 1
+            ? allExpenses[0].amount
+            : allExpenses.reduce((exp1, exp2) => exp1.amount + exp2.amount)
+        );
         setExpenseData(
           allExpenses.sort(
             (exp1, exp2) => new Date(exp2.date) - new Date(exp1.date)
           )
         );
       }
+      if (maxExpense) setUserMaxExpense(maxExpense.amount);
+      else setUserMaxExpense(0);
+      setLoading(false);
     };
 
-    if (data && !isLoading) {
+    if (
+      !sharedExpenseFetcher.isLoading &&
+      !expenseFetcher.isLoading &&
+      !maxExpenseFetcher.isLoading
+    )
       getExpenses();
-    }
-  }, [data, isLoading]);
+  }, [
+    sharedExpenseFetcher.data,
+    expenseFetcher.data,
+    maxExpenseFetcher.data,
+    sharedExpenseFetcher.isLoading,
+    expenseFetcher.isLoading,
+    maxExpenseFetcher.isLoading,
+  ]);
 
-  const routerHistory = useHistory();
+  useEffect(() => {
+    const headCardsData = [
+      {
+        header: 'Total Expenses',
+        amount: `₹${expenseTotalAmount}`,
+        percentage: '2.5%',
+        icon: <Fa.FaRegMoneyBillAlt className={moneyIconClass} />,
+        iconParentClass: 'bg-green-100 rounded-full p-2.5 self-center',
+        get badge() {
+          const percentageIncDec = this.percentage.split('%')[0];
+          return handleBadges(
+            percentageIncDec > 0 ? 'up' : 'down',
+            percentageIncDec > 0 ? 'down' : 'up'
+          );
+        },
+      },
+      {
+        header: 'Remaining Savings',
+        amount:
+          userMaxExpense !== 0 ? (
+            `₹${userMaxExpense - expenseTotalAmount}`
+          ) : (
+            <button
+              className='h-max w-full text-xl font-bold rounded-md text-blue-700'
+              onClick={() => routerHistory.push('/settings/profile')}>
+              Set max expense
+            </button>
+          ),
+        percentage: userMaxExpense
+          ? `${(expenseTotalAmount / userMaxExpense) * 100}%`
+          : '0',
+        icon: <Bi.BiWallet className={walletIconClass} />,
+        iconParentClass: 'bg-indigo-100 rounded-full p-2.5 self-center',
+        get badge() {
+          const percentageIncDec = this.percentage.split('%')[0];
+          return handleBadges(
+            percentageIncDec > 0 ? 'down' : 'up',
+            percentageIncDec > 0 ? 'down' : 'up'
+          );
+        },
+      },
+      {
+        header: (
+          <button
+            className='text-base font-semibold'
+            onClick={handleLendBorrowToggle}>
+            {lendBorrowToggle ? (
+              <span className='flex items-center'>
+                Total Borrowing{' '}
+                <Hi.HiChevronDown className='ml-1.5 h-4 w-4 mt-0.5' />
+              </span>
+            ) : (
+              <span className='flex items-center'>
+                Total Lending{' '}
+                <Hi.HiChevronUp className='ml-1.5 h-4 w-4 mt-0.5' />
+              </span>
+            )}
+          </button>
+        ),
+        amount: lendBorrowToggle ? `₹${borrow}` : `₹${lend}`,
+        percentage: '0',
+        icon: <Ri.RiHandCoinLine className={ownedIconClass} />,
+        iconParentClass: 'bg-red-100 rounded-full p-2.5 self-center',
+        get badge() {
+          return handleBadges(this.percentage.split('%')[0]);
+        },
+      },
+    ];
 
-  /*   const toastRef = useRef();
+    setHeadCards(headCardsData);
+  }, [userMaxExpense, expenseTotalAmount, lendBorrowToggle]);
+
+  const toastRef = useRef();
 
   const addToast = (mainMessage, subMessage, icon) => {
     toastRef.current.addMessage({
@@ -65,65 +213,23 @@ export const Main = () => {
       subMessage: subMessage,
       icon: icon,
     });
-  }; */
+  };
 
   const mainIconClass = 'h-10 w-10';
   const moneyIconClass = String(`${mainIconClass} text-green-400`);
   const walletIconClass = String(`${mainIconClass} text-indigo-400`);
   const ownedIconClass = String(`${mainIconClass} text-red-400`);
 
-  const badgeMainClass = 'flex font-semibold px-1.5 py-0.5 rounded-full';
-
-  const handleBadges = (percentage, relation = 'normal') => {
-    if (percentage === 0) return '';
-    else {
-      if (relation === 'normal' ? percentage > 0 : percentage < 0) {
-        return {
-          badgeClass: `${badgeMainClass}  bg-red-100 text-red-500`,
-          badgeIcon:
-            relation === 'normal' ? <Hi.HiChevronUp /> : <Hi.HiChevronDown />,
-        };
-      } else
-        return {
-          badgeClass: `${badgeMainClass}  bg-green-100 text-green-500`,
-          badgeIcon:
-            relation === 'normal' ? <Hi.HiChevronDown /> : <Hi.HiChevronUp />,
-        };
-    }
+  const handleBadges = (arrowDirection, trend) => {
+    return {
+      badgeClass:
+        trend === 'up'
+          ? 'flex font-semibold px-1.5 py-0.5 rounded-full bg-green-100 text-green-500'
+          : 'flex font-semibold px-1.5 py-0.5 rounded-full bg-red-100 text-red-500',
+      badgeIcon:
+        arrowDirection === 'up' ? <Hi.HiTrendingUp /> : <Hi.HiTrendingDown />,
+    };
   };
-
-  const headCards = [
-    {
-      header: 'Total Expenses',
-      amount: '₹1000.00',
-      percentage: '2.5%',
-      icon: <Fa.FaRegMoneyBillAlt className={moneyIconClass} />,
-      iconParentClass: 'bg-green-100 rounded-full p-2.5 self-center',
-      get badge() {
-        return handleBadges(this.percentage.split('%')[0]);
-      },
-    },
-    {
-      header: 'Total Savings',
-      amount: '₹500.00',
-      percentage: '5%',
-      icon: <Bi.BiWallet className={walletIconClass} />,
-      iconParentClass: 'bg-indigo-100 rounded-full p-2.5 self-center',
-      get badge() {
-        return handleBadges(this.percentage.split('%')[0], 'inverse');
-      },
-    },
-    {
-      header: 'Total Active Owing',
-      amount: '₹0.00',
-      percentage: '0',
-      icon: <Ri.RiHandCoinLine className={ownedIconClass} />,
-      iconParentClass: 'bg-red-100 rounded-full p-2.5 self-center',
-      get badge() {
-        return handleBadges(this.percentage.split('%')[0]);
-      },
-    },
-  ];
 
   const handleSearchBar = () => {
     setToggleSearch(!toggleSearch);
@@ -141,12 +247,6 @@ export const Main = () => {
     return routerHistory.push(path);
   };
 
-  const iter = [1];
-  let sum = 0;
-  const addAmount = (amount) => {
-    sum += amount;
-  };
-
   return (
     <>
       <Helmet>
@@ -156,19 +256,19 @@ export const Main = () => {
         {/* Top Cards */}
         <div className='lg:h-1/5 py-5 lg:grid lg:grid-cols-3 lg:space-x-4 lg:space-y-0 lg:grid-rows-1 grid-cols-1 grid-rows-3 space-x-0 space-y-4'>
           {headCards.map((card) => (
-            <div className='flex justify-between bg-white rounded-xl border border-gray-200 shadow-md px-5 py-2.5'>
-              <div>
-                <h1 className='font-poppins font-semibold text-xl '>
+            <div className='flex justify-between items-center bg-white rounded-xl border border-gray-200 shadow-md px-5 py-3.5'>
+              <div className='flex-1'>
+                <h1 className='font-poppins font-semibold text-base text-gray-500'>
                   {card.header}
                 </h1>
-                <div className='flex items-center mt-2'>
-                  <h2 className='text-lg font-bold text-gray-500 mr-2.5'>
+                <div className='flex items-center mt-0.5 proportional-nums'>
+                  <h2 className='text-2xl font-bold text-gray-800 mr-2.5'>
                     {card.amount}
                   </h2>
                   {card.percentage !== '0' ? (
                     <div className={card.badge.badgeClass}>
                       {card.badge.badgeIcon}
-                      <h2 className='text-xs '>{card.percentage}</h2>
+                      <h2 className='text-xs ml-0.5'>{card.percentage}</h2>
                     </div>
                   ) : (
                     <></>
@@ -277,7 +377,6 @@ export const Main = () => {
                           <div className='w-1/4 font-bold flex justify-between items-center'>
                             <span className='text-gray-700'>
                               ₹{expense.amount}
-                              {addAmount(expense.amount)}
                             </span>
                             <Hi.HiChevronRight className='mr-1 h-5 w-5 text-current' />
                           </div>
@@ -294,7 +393,9 @@ export const Main = () => {
                     </div>
                     <div className='flex flex-col w-1/4'>
                       <h4 className='text-gray-700'>Total Amount</h4>
-                      <span className='text-gray-500'>₹{sum}</span>
+                      <span className='text-gray-500'>
+                        ₹{expenseTotalAmount}
+                      </span>
                     </div>
                   </div>
                 </>
@@ -331,8 +432,8 @@ export const Main = () => {
               </div>
             </div>
             <div className='flex-1 overflow-y-auto md:px-5 px-3.5'>
-              {iter.map(() => (
-                <div className='flex justify-between rounded-lg py-2 px-2.5 pl-5 hover:bg-gray-100 mb-2.5 cursor-pointer'>
+              {[1, 2, 3, 4, 5, 6, 7].map(() => (
+                <div className='flex justify-between rounded-xl py-3 px-2.5 pl-5 hover:bg-gray-100 mb-0.5 cursor-pointer'>
                   <h5 className='text-sm text-gray-700 font-bold'>2020</h5>
                   <div className='text-sm flex items-center'>
                     <h5 className='text-gray-700 mr-2.5 font-bold'>₹1000.00</h5>
@@ -402,8 +503,8 @@ export const Main = () => {
             </button>
           </div> */}
       </div>
-      {/* <ToastPortal ref={toastRef} autoClose={true} />
-      <ModalPortal ref={modalRef} /> */}
+      <ToastPortal ref={toastRef} autoClose={true} />
+      <ModalPortal ref={modalRef} />
     </>
   );
 };
