42d0af0b2c11dbd4091b904fbd09519f24f7def7
targusrock <targusrock@gmail.com>
2021-12-26 12:40:07 +0530

    Updated: UserContext to tsx

---
+++ frontend/src/context/UserContext.tsx
@@ -0,0 +1,36 @@
+import { FC, createContext, useEffect, useState } from 'react';
+import { useHistory } from 'react-router-dom';
+
+export const UserContext = createContext<Array<any>>([]);
+
+export const UserProvider: FC = ({ children }) => {
+  const [token, setToken] = useState<string | null>(localStorage.getItem('userToken'));
+  const [isAuthenticated, setIsAuthenticated] = useState<boolean>(false);
+  const history = useHistory();
+
+  useEffect(() => {
+    if (token) {
+      localStorage.setItem('userToken', token);
+      setIsAuthenticated(true);
+    }
+    setTimeout(() => {
+      setIsAuthenticated(false);
+      localStorage.removeItem('userToken');
+      history.push('/');
+    }, 1800000);
+  }, [token]);
+
+  const logout = (): void => {
+    localStorage.setItem('userToken', '');
+    setIsAuthenticated(false);
+    localStorage.clear();
+    history.push('/');
+  };
+
+  return (
+    <UserContext.Provider
+      value={[token, setToken, isAuthenticated, setIsAuthenticated, logout]}>
+      { children }
+    </UserContext.Provider>
+  );
+};
